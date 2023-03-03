b55e27dbd78f78965e70d9108dedada5020a5dc9
cauenogueir <cauenogueiraj@gmail.com>
2023-03-02 22:25:15 -0300

    aula laravel

---
+++ laravel/resources/views/principal.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<h1> Ola mundo </h1>
+
+    <nav>
+        <ul>
+            <li> <a href="/"> Welcome </a> <li>
+            <li> <a href="/contato"> Contato </a></li>
+            </li> <a href="/quemsomos">< Quem somos </a> </li>
+            </li> <a href="/laboratorio">< laboratorio </a> </li>
+            </li> <a href="/reclamacao">< reclamacao </a> </li>
+     </ul>
+</nav>
+    
+</body>
+</html>
---
+++ laravel/routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('principal');
+});
+
+Route::get('/contato', function () {
+    return view('contato');
+});
+
+Route::get('/quemsomos', function () {
+    return view('quem-somos');
+});
+
+Route::get('/laboratorio', function () {
+    return view('laboratorio');
+});
+
+Route::get('/reclamacao', function () {
+    return view('reclamacao');
+});
+
